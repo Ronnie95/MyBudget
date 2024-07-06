ba106f29502580ce9f3c01078d6c532a108aa659
Ronnie95 <t_daron@yahoo.com>
2024-07-05 23:44:20 -0400

    update

--- my_budget/budget_app/urls.py
+++ my_budget/budget_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+# from .models import Goals, Reminders, Reports, Transactions, Income
 
-# this like app.use() in express
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
+    path('', views.GoalList.as_view(), name="goals"), # <- here we have added the new path
 
 ]
--- my_budget/budget_app/views.py
+++ my_budget/budget_app/views.py
@@ -5,9 +5,19 @@ from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
+from .models import Goals, Reminders, Reports, Transactions, Income
+
 
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
 class Home(TemplateView):
     template_name = "home.html"
+
+class GoalList(TemplateView):
+    template_name = "goals.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["Goals"] = Goals.objects.all() # Here we are using the model to query the database for us.
+        return context
