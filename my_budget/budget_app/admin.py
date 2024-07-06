bc9c8733676cc450b38f46275983616b6306f5bd
Ronnie95 <t_daron@yahoo.com>
2024-07-05 23:28:16 -0400

    updates

--- my_budget/budget_app/admin.py
+++ my_budget/budget_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Income, Transactions, Goals, Reminders, Reports
+
+
+admin.site.register(Income)
+admin.site.register(Transactions)
+admin.site.register(Goals)
+admin.site.register(Reminders)
+admin.site.register(Reports)
 
 # Register your models here.
--- my_budget/budget_app/models.py
+++ my_budget/budget_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Income(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
 # class Categories(models.Model):
     
@@ -26,18 +29,24 @@ class Transactions(models.Model):
         "Investments": "Investments",
         "Misc": "Misc"
     }
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
 class Goals(models.Model):
     goal_name = models.CharField(max_length=100)
     target_amount = models.IntegerField()
     target_date = models.DateField()
     notes = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
 class Reports(models.Model):
     total_income = models.IntegerField()
     total_expenses = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
 
 class Reminders(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     notes = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
